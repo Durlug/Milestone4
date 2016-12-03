1e77cb5a4ad35c6e82c3f5b58e0cc58b48b3b514
Durlug <shaun.g.richardson@gmail.com>
2016-12-03 04:11:21 -0500

    Add files via upload

---
+++ CustomMade.cpp
@@ -0,0 +1,107 @@
+//Milestone 4
+//CustomMade.cpp
+//Shaun Richardson
+//110392156
+//srichardson6@myseneca.ca
+
+#include "CustomMade.h"
+
+namespace ict 
+{
+
+	CustomMade::CustomMade(): OnShelf('C')
+	{
+	}
+
+	const Date& CustomMade::delivery() const
+	{
+		return delivery_;
+	}
+
+	void CustomMade::delivery(const Date &value)
+	{
+		delivery_ = value;
+	}
+
+	std::fstream& CustomMade::store(std::fstream& file, bool linear)
+	{
+		OnShelf::store(file, false);
+		if (linear == true)
+		{
+			file << ',' << delivery_ << std::endl;
+		}
+		else
+		{
+			file << ',' << delivery_;
+		}
+		return file;
+	}
+
+	std::fstream& CustomMade::load(std::fstream& file)
+	{
+		OnShelf::load(file);
+		delivery_.read(file);
+		file.ignore(1);
+		return file;
+	}
+
+	std::ostream& CustomMade::display(std::ostream& ostr, bool linear)
+	{
+		OnShelf::display(ostr, linear);
+		if (err_.isClear()) 
+		{
+			if (linear == true)
+			{
+				ostr << delivery_;
+			}
+			else
+			{
+				ostr << std::endl << "Delivery Date: " << delivery_;
+			}
+			return ostr;
+
+		}
+	}
+
+	std::istream& CustomMade::conInput(std::istream& istr)
+	{
+		Date temp;
+		OnShelf::conInput(istr);
+
+		if (err_.isClear())
+		{
+			std::cout << "Delivery Date (YYYY/MM/DD):";
+			istr >> temp;
+
+			if (temp.bad())
+			{
+				if (temp.errCode() == CIN_FAILED)
+				{
+					err_.message("Invalid Date Entry");
+				}
+				else if (temp.errCode() == YEAR_ERROR)
+				{
+					err_.message("Invalid Year in Date Entry");
+				}
+				else if (temp.errCode() == MON_ERROR)
+				{
+					err_.message("Invalid Month in Date Entry");
+				}
+				else if (temp.errCode() == DAY_ERROR)
+				{
+					err_.message("Invalid Day in Date Entry");
+				}
+				istr.setstate(std::ios::failbit);
+			}
+
+		}
+		else
+		{
+			delivery(temp);
+		}
+		return istr;
+	}
+
+
+
+}
---
+++ CustomMade.h
@@ -0,0 +1,29 @@
+//Milestone 4
+//CustomMade.h
+//Shaun Richardson
+//110392156
+//srichardson6@myseneca.ca
+
+#ifndef ICT_CUSTOMMADE_H__
+#define ICT_CUSTOMMADE_H__
+
+#include "OnShelf.h"
+#include "Date.h"
+#include "Error.h"
+namespace ict {
+   class CustomMade :public OnShelf{
+   private:
+	   Date delivery_;
+   public:
+	   CustomMade();
+	   const Date& delivery() const;
+       void delivery(const Date &value);
+	   std::fstream& store(std::fstream& file, bool linear = true);
+	   std::fstream& load(std::fstream& file);
+	   std::ostream& display(std::ostream& ostr, bool linear);
+	   std::istream& conInput(std::istream& istr);
+   };
+}
+
+
+#endif
